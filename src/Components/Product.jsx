de69218b686f4f25557f85cafd89d7112dafbcfa
jasmin <jasminchakma895@gmail.com>
2024-02-20 15:48:45 +0600

    product add to card successfully and save to db

---
+++ src/Components/Product.jsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Product = ({ product }) => {
+  const handleAddToCart = async() => {
+    const res = await axios.post(`http://localhost:5000/addToCart`, product);
+    if (res.data.acknowledged) {
+        alert('cart added successfully')
+    }
+  };
+
+  return (
+    <Link to={`/details/${product._id}`}>
+      <div className="flex flex-col">
+        <figure>
+          <img src={product.image} alt="Shoes" className="h-64 w-full" />
+        </figure>
+        <div className="text-center mt-3">
+          <h2 className="text-lg font-medium">{product.name}</h2>
+          <p>Rating: {product.rating}</p>
+          <p className="font-medium">Price : $200</p>
+        </div>
+        <div className="text-center mt-5">
+          <button
+            onClick={() => handleAddToCart()}
+            className="py-2 px-3 border text-gray-500 hover:bg-green-500 hover:text-white uppercase font-medium"
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Product;
--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,62 +1,25 @@
-import { Link, useLoaderData } from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
+import Product from "./Product";
 
 const Products = () => {
   const allProducts = useLoaderData();
+
+ 
   return (
     <div className="mt-10 px-5 md:px-0">
       <div>
         <p className="text-2xl font-bold mb-5">New Products</p>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
-          {allProducts.slice(0, 4).map((product, idx) => (
-            <Link to={`/details/${product._id}`} key={idx}>
-              <div className="flex flex-col">
-                <figure>
-                  <img
-                    src={product.image}
-                    alt="Shoes"
-                    className="h-64 w-full"
-                  />
-                </figure>
-                <div className="text-center mt-3">
-                  <h2 className="text-lg font-medium">{product.name}</h2>
-                  <p>Rating: {product.rating}</p>
-                  <p className="font-medium">Price : $200</p>
-                </div>
-                <div className="text-center mt-5">
-                  <button className="py-2 px-3 border text-gray-500 hover:bg-green-500 hover:text-white uppercase font-medium">
-                    Add To Cart
-                  </button>
-                </div>
-              </div>
-            </Link>
+          {allProducts.slice(0, 4).map((product) => (
+            <Product key={product._id} product={product}></Product>
           ))}
         </div>
       </div>
       <div className="my-10">
         <p className="text-2xl font-bold mb-5 ">Fetured Products</p>
         <div className="grid grid-cols-1 md:grid-cols-4 gap-5">
-          {allProducts.map((product, idx) => (
-            <Link to={`/details/${product._id}`} key={idx}>
-              <div className="flex flex-col">
-                <figure>
-                  <img
-                    src={product.image}
-                    alt="Shoes"
-                    className="h-64 w-full"
-                  />
-                </figure>
-                <div className="text-center mt-3">
-                  <h2 className="text-lg font-medium">{product.name}</h2>
-                  <p>Rating: {product.rating}</p>
-                  <p className="font-medium">Price : $200</p>
-                </div>
-                <div className="text-center mt-5">
-                  <button className="p-2 border text-gray-500 hover:bg-green-500 hover:text-white uppercase font-medium">
-                    Add To Cart
-                  </button>
-                </div>
-              </div>
-            </Link>
+          {allProducts.map((product) => (
+            <Product key={product._id} product={product}></Product>
           ))}
         </div>
       </div>
