53b6ac9c84f55a3bdd34a76a4309b00ac9fd845a
jasmin <jasminchakma895@gmail.com>
2024-02-21 16:55:43 +0600

    banner text changed

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -9,10 +9,9 @@ const Banner = () => {
     >
       <div className="text-left absolute inset-0 flex items-center justify-start px-5">
         <div className="mt-20 md:ps-10">
-          <p className="text-white font-medium text-3xl">Hello My Friend</p>
+          <p className="text-white font-medium text-3xl">Discover Your Style Destination</p>
           <p className="w-1/2 text-white">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Expedita
-            totam distinctio accusamus porro mollitia odit eveniet
+          Welcome to ZIOR, your go-to destination for fashion-forward clothing and accessories. Discover the latest trends, timeless classics, and everything in between. With curated collections for every occasion,{` we've`} got your style needs covered. Shop now and elevate your wardrobe to new heights!
           </p>
           <button className="py-2 px-3 bg-white mt-3 font-semibold">Shop Now</button>
         </div>
