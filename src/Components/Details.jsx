612c32716552e1357ec511f555e068e09100f32b
jasmin <jasminchakma895@gmail.com>
2024-02-21 14:06:19 +0600

    details img issu solved

--- src/Components/Details.jsx
+++ src/Components/Details.jsx
@@ -5,17 +5,16 @@ const Details = () => {
   const {_id, image, name, type, price, rating, brand, details } = detailsProduct;
 
   return (
-    <div className="md:flex flex-row px-5 md:p-0 items-center my-20 justify-between gap-5">
-      <div className="flex-1 rounded-lg border-gray-300 border-2">
+    <div className="md:flex flex-row px-5 md:p-0 items-center my-20 gap-5">
+      <div className="rounded-lg border-gray-300 border-2 flex-1">
         <img src={image} alt="" className="h-[60vh] w-full" />
       </div>
-      <div>
+      <div className="flex-1">
         <p className="text-2xl font-semibold">{name}</p>
         <p className="font-medium">Type : {type}</p>
         <p className="font-medium">Price : {price}</p>
         <p className="font-medium">Rating : {rating}</p>
         <p className="font-medium">Brand : {brand}</p>
-        <p>product id {_id}</p>
         <p>{details}</p>
         <div className="mt-3 ">
           <Link to={`/updateProduct/${_id}`}>
